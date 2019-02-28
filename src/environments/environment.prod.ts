35fae809a99dc14472e7d2e56836b6411a8f6512
mlabenski <mlabenski123@gmail.com>
2019-02-27 19:01:40 -0500

    server up on heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDmhXO3nbvoRsnqhk1Wcy8q-9NPPdqUVdY",
+    authDomain: "geeb-off.firebaseapp.com",
+    databaseURL: "https://geeb-off.firebaseio.com",
+    projectId: "geeb-off",
+    storageBucket: "geeb-off.appspot.com",
+    messagingSenderId: "858898825318"
+  }
 };
